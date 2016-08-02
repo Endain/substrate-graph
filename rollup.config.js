24a108f3f8d03788fcc25fb66cc66d492765ad13
Endain <endain.gaming@gmail.com>
2016-08-02 01:40:45 -0700

    Updated rollup build
    Added minification to rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,13 @@
+import uglify from 'rollup-plugin-uglify';
+import { minify } from 'uglify-js2';
+
+
 export default {
     'entry': 'index.js',
     'format': 'cjs',
     'dest': 'build/substrate-graph.js',
-    'indent': true
+    'indent': true,
+    plugins: [
+        uglify( {}, minify )
+    ]
 };
